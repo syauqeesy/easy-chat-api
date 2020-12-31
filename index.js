314b5acc764e6cc390c108398068a293f958a5a0
syauqeesy <syauqeesy@gmail.com>
2020-12-31 11:21:09 +0700

    Setup sequelize

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 
+const { sequelize } = require('./models')
+
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 io.on('connection', _ => {
 
 })
 
-server.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
+server.listen(process.env.PORT, async () => {
+  await sequelize.authenticate()
+  console.log(`Server running on port ${process.env.PORT}`)
+})
